9526c0485a3c214a3a768fb4983420e699d9b0b8
Mad Bernard <madeline.a.bernard@gmail.com>
2015-12-01 11:00:44 -0800

    rainbow pulsing square works, have color changing library

--- src/dancer.js
+++ src/dancer.js
@@ -41,7 +41,7 @@ var Dancer = function(top, left, timeBetweenSteps) {
   this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
 
-  this.step();
+  this.step.call(this);
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
---
+++ src/rainbowDancer.js
@@ -0,0 +1,35 @@
+// a dancer that changes color each step
+
+var RainbowDancer = function(top, left, timeBetweenSteps) {
+  this.colorList = ['aqua', 'lime', 'fuchsia'];
+  this.currentColorIndex = 0; //possibly change to random
+  this.expanding = true;
+  this.maxSize = 10 + Math.random() * 30;
+  Dancer.call(this, top, left, timeBetweenSteps);
+  //this.timeBetweenSteps is some other random number
+};
+
+RainbowDancer.prototype = Object.create(Dancer.prototype);
+RainbowDancer.prototype.constructor = RainbowDancer;
+
+RainbowDancer.prototype.step = function() {
+  if (this.currentColorIndex === this.colorList.length - 1) {
+    this.currentColorIndex = 0;
+  }
+  else {
+    this.currentColorIndex++;
+  }
+
+  if (this.expanding) {
+    this.$node.animate({'borderWidth': this.maxSize + 'px' }, this.timeBetweenSteps);
+    this.expanding = false;
+  }
+  else {
+    this.$node.animate({'borderWidth': '10px' }, this.timeBetweenSteps);
+    this.expanding = true;
+  }
+
+  this.$node.animate({'borderColor' : this.colorList[this.currentColorIndex] }, this.timeBetweenSteps);
+  Dancer.prototype.step.call(this);
+};
+
